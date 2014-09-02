1dda0bb1a30b8d9bf668483d3e33d9533a7677ab
Cater <buptjialin@icloud.com>
2014-09-02 23:19:13 +0800

    1

---
+++ flowControl.py
@@ -0,0 +1,90 @@
+import urllib2
+import urllib
+import re
+import time
+Limit=10485760
+cnt=0
+def login(ID,passWord):
+	#print "login with "+ID+"\n"
+	info = {'DDDDD':ID,
+	'upass':passWord,
+	'0MKKey':'123456'}
+	key = urllib.urlencode(info)
+	url = "http://gw.bupt.edu.cn"
+	door = urllib2.Request(url,key)
+	#print door
+	res = urllib2.urlopen(door).read()
+	#print res
+
+def logout():
+	url = "http://gw.bupt.edu.cn/F.htm"
+	door = urllib2.Request(url)
+	res = urllib2.urlopen(door).read()
+	#print res
+
+def getFlow():
+	url = "http://gw.bupt.edu.cn/"
+	door = urllib2.Request(url)
+	res = urllib2.urlopen(door).read()
+	#print res
+	l = re.search(r'flow=\'(\d+)',res)
+	if l:
+		n = l.group(0)
+	else:
+		#print "login unsuccess, next ID\n"
+		return -1
+	m = re.search(r'\d+',n)
+	if m:
+		r=m.group(0)
+		return int(r)
+	else:
+		#print "login unsuccess, next ID\n"
+		return -1
+	
+
+def newLogin():
+	info = {'DDDDD':'2013213317',
+	'upass':'f25407541d57499a530e984c3f844a65123456781',
+	'R1':'0',
+	'R2':'1',
+	'para':'00',
+	'0MKKey':'123456'}
+	key = urllib.urlencode(info)
+	url = "http://gw.bupt.edu.cn"
+	door = urllib2.Request(url,key)
+	res = urllib2.urlopen(door).read()
+	print res
+
+def readIDList(cnt):
+	file_object = open('s.log')
+	all_the_text = file_object.read()
+	file_object.close()
+	line=all_the_text.split('\n')[cnt]
+	info=line.split(' ')
+	ID=info[0]
+	passWord=info[1]
+	print "now login with ID "+ID
+	login(ID,passWord)
+
+def test():
+	flow=getFlow()
+	#print flow
+	if flow<0 or flow>=Limit:
+		global cnt
+		cnt+=1
+		print "login unsuccess or flow execced\n"
+		return False
+	else:
+		print "check success"
+		return True
+
+def fuckYourSelf():
+	# read ID list
+	logout()
+	while True:
+		global cnt
+		readIDList(cnt)
+		while test():
+			time.sleep(3)
+
+fuckYourSelf()
